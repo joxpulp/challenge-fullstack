d8eadcd0cbfec7932650c4e8b8fe61c8af9a5550
joxpulp <joxpulp@gmail.com>
2021-11-04 12:10:42 -0500

    chore: deleted unused files

--- server/src/middlewares/validate.ts
+++ server/src/middlewares/validate.ts
@@ -1,15 +1,20 @@
 import { NextFunction, Request, Response } from 'express';
 import { OptionalObjectSchema } from 'yup/lib/object';
+import cloudinary from '../services/cloudinary';
 
 export const validate =
-	(schema: OptionalObjectSchema<{}>) => async (req: Request, res: Response, next: NextFunction) => {
+	(schema: OptionalObjectSchema<{}>) =>
+	async (req: Request, res: Response, next: NextFunction) => {
 		try {
 			await schema.validate({
 				body: req.body,
-			})
+			});
 			return next();
 		} catch (err) {
 			if (err instanceof Error) {
+				if (req.file) {
+					await cloudinary.uploader.destroy(req.file!.filename);
+				}
 				res.status(400).json({ error: err.message });
 			}
 		}
